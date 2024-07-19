a65a745fe6fe9cd57d91e2dfa951d130120d1f6c
yash-visavadiya-tark <yash.visavadiya@tarktech.com>
2024-07-19 10:25:32 +0530

    added Daily question of LeetCode => 19-07-2024

---
+++ Lucky Numbers in a Matrix.py
@@ -0,0 +1,19 @@
+﻿from typing import List
+
+
+class Solution:
+    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+        lucky_numbers = []
+
+        for row in matrix:
+            min_index = row.index(min(row))
+            min_value = row[min_index]
+
+            if all(row[min_index] >= matrix[i][min_index] for i in range(len(matrix))):
+                lucky_numbers.append(min_value)
+
+        return lucky_numbers
+
+
+if __name__ == '__main__':
+    print(Solution().luckyNumbers([[3, 7, 8], [9, 11, 13], [15, 16, 17]]))  # [15]
