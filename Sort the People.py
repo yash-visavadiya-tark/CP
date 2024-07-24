132052add47d5a9ec59b293f626a4a253be80094
yash-visavadiya-tark <yash.visavadiya@tarktech.com>
2024-07-24 11:05:25 +0530

    added Daily question of LeetCode => 22-07-2024

---
+++ Sort the People.py
@@ -0,0 +1,11 @@
+﻿from typing import List
+
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [name for name, _ in sorted(zip(names, heights), key=lambda x: -x[1])]
+
+
+if __name__ == '__main__':
+    print(Solution().sortPeople(["Mary", "John", "Emma"], [180, 165, 170]))  # ["Mary","Emma","John"]
+    print(Solution().sortPeople(["Alice", "Bob", "Bob"], [155, 185, 150]))  # ["Bob","Alice","Bob"]
