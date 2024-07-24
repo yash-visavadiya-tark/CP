527cdd442cf5a3c0ffe23d733654a664ef1ee939
yash-visavadiya-tark <yash.visavadiya@tarktech.com>
2024-07-24 11:05:34 +0530

    added Daily question of LeetCode => 23-07-2024

---
+++ Sort Array by Increasing Frequency.py
@@ -0,0 +1,14 @@
+﻿from collections import Counter
+from typing import List
+
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        counter = Counter(nums)
+        return sorted(nums, key=lambda x: (counter[x], -x))
+
+
+if __name__ == '__main__':
+    print(Solution().frequencySort([1, 1, 2, 2, 2, 3]))  # [3,1,1,2,2,2]
+    print(Solution().frequencySort([2, 3, 1, 3, 2]))  # [1,3,3,2,2]
+    print(Solution().frequencySort([-1, 1, -6, 4, 5, -6, 1, 4, 1]))  # [5,-1,4,4,-6,-6,1,1,1]
